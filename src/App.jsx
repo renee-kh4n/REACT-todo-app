13c3bdb3c4e5d799787bbc25230f5d13fe08004e
renee-kh4n <renee.a.f.khan@gmail.com>
2024-05-22 10:29:00 +0800

    changed some styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,5 +13,16 @@ export default function App(){
   </div>
   <button className="btn">Add</button>
 </form>
-<h1 className="header">My To Do List</h1></>
+<h1 className="header">My To Do List</h1>
+<ul className="list">
+  <li>
+    <label>
+      <input type="checkbox"/>
+      Item 1
+    </label>
+    <button className="btn btn-danger">Delete</button>
+  </li>
+</ul>
+</>
+
 }
